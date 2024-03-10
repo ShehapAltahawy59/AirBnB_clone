972af1c66fd1b551039948c555598bbd286f76d4
shehap62 <shehapkhalil62@gmail.com>
2024-03-10 13:14:57 +0200

    edit

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest for the Console class
+Test files by using the following command line:
+python3 -m unittest tests/test_console.py
+"""
+import unittest
+from os import path, remove
+
+
+class TestConsole(unittest.TestCase):
+    """define variables and methods"""
+    pass

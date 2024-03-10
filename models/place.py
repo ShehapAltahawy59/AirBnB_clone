19cdbf49fcbe316d15ba4e3f1cea1ea085c36fd9
shehap62 <shehapkhalil62@gmail.com>
2024-03-10 10:37:02 +0200

    edit

--- models/place.py
+++ models/place.py
@@ -1,10 +1,14 @@
-#!/usr/bin/python3
-""" Place Module for HBNB project """
+#!/usr/bin/env python3
+"""class place inheriting from BaseModel
+"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """ A place to stay """
+    """class place inheriting from BaseModel
+    """
+
     city_id = ""
     user_id = ""
     name = ""
@@ -15,4 +19,4 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    
+    amenity_ids = []

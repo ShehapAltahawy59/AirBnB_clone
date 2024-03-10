2352519afe4775890dfd9edbbb20d2835f6e293a
shehap62 <shehapkhalil62@gmail.com>
2024-03-10 09:39:34 +0200

    edit

--- test_base_model.py
+++ test_base_model.py
@@ -7,8 +7,9 @@ my_model.my_number = 89
 print(my_model)
 my_model.save()
 print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
+json = my_model.to_dict()
+print(json)
 print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+for key in json.keys():
+    s = "\t{}: ({}) - {}".format(key, type(json[key]), json[key])
+    print(s)
